9c7342e3b7a9b85d45d2f9a08daea821681957c8
Smruti Prakash Rout <smrutiprakashrout3@gmail.com>
2024-01-21 20:29:35 +0530

    Your commit message

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-red-600 text-center">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App;
