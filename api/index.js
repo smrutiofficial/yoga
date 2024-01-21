3180afab52614e63c2ad6e9d2cae208265dfd448
Smruti Prakash Rout <smrutiprakashrout3@gmail.com>
2024-01-22 00:12:58 +0530

    connect db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
-const app=express();
-app.listen(3000,()=>{
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const uri = process.env.URI ;
+
+mongoose.connect(uri).then(()=>{
+    console.log("connected to mongodb");
+}).catch((err)=>{
+    console.log(err);
+});
+
+const app = express();
+app.listen(3000, () => {
     console.log('Server started at on port 3000');
 })
